11cc7631e765df537c5a9e56bf64184a52202b08
nuomi1 <nuomi1@qq.com>
2020-12-16 23:31:08 +0800

    feat: init

---
+++ BitrateViewer/BitrateViewerApp.swift
@@ -0,0 +1,17 @@
+//
+//  BitrateViewerApp.swift
+//  BitrateViewer
+//
+//  Created by nuomi1 on 2020/12/16.
+//
+
+import SwiftUI
+
+@main
+struct BitrateViewerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ BitrateViewer/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  BitrateViewer
+//
+//  Created by nuomi1 on 2020/12/16.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
